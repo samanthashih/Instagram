e96d746e0c87ba4dfa0d8eac5271af5114defbf4
Samantha Shih <sam.shih21@gmail.com>
2022-06-13 15:18:14 -0700

    login, logout

---
+++ app/src/main/java/com/example/instagram/LoginActivity.java
@@ -0,0 +1,72 @@
+package com.example.instagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class LoginActivity extends AppCompatActivity {
+    public static final String TAG = "LoginActivity";
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //when app is opened
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        if (ParseUser.getCurrentUser() != null) { // if someone is signed in
+            goMainActivity(); // go straight to main activity
+        }
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "clicked login");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                loginUser(username, password);
+            }
+        });
+
+    }
+
+    private void loginUser(String username, String password) {
+        Log.i(TAG, "Login attempt for user: " + username);
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e!= null) {
+                    // todo: make better error messages to tell user what's wrong
+                    Log.e(TAG, "issue with login", e);
+                    return;
+                }
+                // login good! go to main activity
+                goMainActivity();
+                Toast.makeText(LoginActivity.this, "Login success", Toast.LENGTH_SHORT);
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish(); // finish login activity once logged in so when back button, it won't go back to login
+    }
+}
